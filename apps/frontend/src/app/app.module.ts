ccaf2b3c763b13e7cb4468558137a12ed0180398
Kwandes <bz.jasiek@gmail.com>
2022-08-04 03:52:54 +0200

    Style the navbar

--- apps/frontend/src/app/app.module.ts
+++ apps/frontend/src/app/app.module.ts
@@ -5,17 +5,18 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgModules } from './ng-modules.constant';
 import { CharacterListComponent } from './pages/character-list/character-list.component';
 import { AbilitiesComponent } from './pages/character/abilities/abilities.component';
 import { AdvantagesDisadvantagesComponent } from './pages/character/advantages-disadvantages/advantages-disadvantages.component';
 import { AttributesComponent } from './pages/character/attributes/attributes.component';
 import { CharacterComponent } from './pages/character/character.component';
 import { CombatTechniquesComponent } from './pages/character/combat-techniques/combat-techniques.component';
+import { EquipmentComponent } from './pages/character/equipment/equipment.component';
 import { OverviewComponent } from './pages/character/overview/overview.component';
 import { SkillsComponent } from './pages/character/skills/skills.component';
 import { SpecialAbilitiesComponent } from './pages/character/special-abilities/special-abilities.component';
 import { SpellsComponent } from './pages/character/spells/spells.component';
-import { EquipmentComponent } from './pages/character/equipment/equipment.component';
 
 @NgModule({
   declarations: [
@@ -37,6 +38,7 @@ import { EquipmentComponent } from './pages/character/equipment/equipment.compon
     HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    NgModules,
   ],
   providers: [],
   bootstrap: [AppComponent],
