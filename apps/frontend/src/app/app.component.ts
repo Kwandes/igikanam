365e183d0fad4ede9e92366db6f30458504996ae
Kwandes <bz.jasiek@gmail.com>
2022-09-30 14:54:43 +0200

    Add logout button

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { LocalStorageService, LocalStorageVars } from '@igikanam/local-storage';
+import { AuthService } from './shared/services/auth.service';
 
 @Component({
   selector: 'igikanam-root',
@@ -7,6 +8,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  hello$ = this.http.get<any>('/api/hello');
-  constructor(private http: HttpClient) {}
+  isLoggedIn = false;
+  loggedInCheckInterval!: number;
+  constructor(
+    private authService: AuthService,
+    private localStorageService: LocalStorageService
+  ) {
+    // Set logged in status
+    this.isLoggedIn =
+      this.localStorageService.getItem(LocalStorageVars.accessTokenInfo)
+        ?.value != null;
+    // periodically check the logged in status.
+    // I technically could subscribe to local storage but angular strict rules are making it n annoying process so I went with this meh approach.
+    this.loggedInCheckInterval = window.setInterval(() => {
+      this.updateAccessInfo();
+    }, 2500);
+  }
+
+  /**
+   * Update isLoggedIn and role variables based on the access token info.
+   */
+  updateAccessInfo(): void {
+    this.isLoggedIn =
+      this.localStorageService.getItem(LocalStorageVars.accessTokenInfo)
+        ?.value != null;
+  }
+
+  logout(): void {
+    this.authService.logout();
+  }
 }
